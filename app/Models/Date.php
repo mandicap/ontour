d3820833340f92df7938aa7a623cd052a7696e96
Amanda Romero <mandicap@comcast.net>
2021-11-01 22:48:28 -0600

    Modifies eloquent models

--- app/Models/Date.php
+++ app/Models/Date.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Artist extends Model
+class Date extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['name', 'on_tour'];
 }
--- app/Models/Venue.php
+++ app/Models/Venue.php
@@ -2,41 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Venue extends Model
 {
-    /**
-     * Attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = ['name', 'location'];
-
-    /**
-     * Attributes to be excluded from JSON form of model.
-     *
-     * @var array
-     */
-    protected $hidden = [];
-
-    /**
-     * Attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = [
-        'created_at',
-        'updated_at'
-    ];
-
-    /**
-     * Gets the venue's tours.
-     *
-     * @return void
-     */
-    public function tours()
-    {
-        return $this->belongsToMany(Tour::class)->using(TourVenue::class);
-    }
+    use HasFactory;
 }
